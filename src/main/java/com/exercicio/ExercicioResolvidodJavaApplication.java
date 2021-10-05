152ae288093f1ed914b8196f4aba33f2fdb4bbaa
Jefferson Luiz <jefferson.luiz.cruz@gmail.com>
2021-10-05 10:46:15 -0300

    Primeiro commit

---
+++ src/main/java/com/exercicio/ExercicioResolvidodJavaApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioResolvidodJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioResolvidodJavaApplication.class, args);
+	}
+
+}
