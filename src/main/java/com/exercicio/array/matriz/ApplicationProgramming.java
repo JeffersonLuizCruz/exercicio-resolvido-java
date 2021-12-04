3142dbae48e1e4ef68c61ed0690af45bec03b490
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-12-04 17:23:42 -0300

    resovendo exercicio matriz

--- src/main/java/com/exercicio/array/matriz/ApplicationProgramming.java
+++ src/main/java/com/exercicio/array/matriz/ApplicationProgramming.java
@@ -1,5 +1,7 @@
 package com.exercicio.array.matriz;
 
+import java.util.Scanner;
+
 /**
  * Faça um programa para armazenar em uma matriz os COMPROMISSOS de uma agenda pessoal. 
  * CAda dia do mês deve conter 24 horas, onde para cada uma destas 24 horas podemos 
@@ -9,5 +11,43 @@ package com.exercicio.array.matriz;
  * FORNECENDO O DIA E A HORA PARA OBTER O COMPROMISSO ARMAZENADO.
  * */
 public class ApplicationProgramming {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int[][] compromisso = new int[24][31];
+		
+		boolean condicao = false;
+		byte menu = 0;
+		int dia = 0;
+		int hora = 0;
+		compromisso[dia][hora] = compromisso[dia][hora];
+		while(!condicao) {
+			System.out.println("Insira 1 para agendar compromisso");
+			System.out.println("Insira 2 para consultar compromisso");
+			System.out.println("Insira 0 para sair");
+			
+			menu = input.nextByte();
+			
+			if(Opcao.INSERIR.getValor() == menu) {
+				System.out.println("Informe o dia:");
+				dia = input.nextInt();
+				
+				System.out.println("Informe a hora do compromisso:");
+				hora = input.nextInt();
+				
+				if(dia >= 31 && hora >= 24) {
+					compromisso[dia][hora] = compromisso[dia][hora];
+				}
+			} else if(Opcao.CONSULTAR.getValor() == menu) {
+				System.out.println("Consulta");
+				System.out.println("dia e hora " + compromisso[dia][hora]);
+			} else if(Opcao.SAIR.getValor() == menu) {
+				System.out.println("Sair");
+				condicao = true;
+			} else {
+				System.out.println("Opção inválida, informe novamente");
+			}
+		}
+	}
 
 }
---
+++ src/main/java/com/exercicio/array/matriz/Opcao.java
@@ -0,0 +1,19 @@
+package com.exercicio.array.matriz;
+
+public enum Opcao {
+	INSERIR(1),
+	CONSULTAR(2),
+	SAIR(0);
+	
+	private int valor;
+	
+	private Opcao() {}
+
+	private Opcao(int valor) {
+		this.valor = valor;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+}
