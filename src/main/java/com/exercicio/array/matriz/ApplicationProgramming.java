b9176cb30d03bfec2ba9a425caf38245b3bfdcc8
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-12-04 15:28:13 -0300

    Adiciona exercício de matriz

---
+++ src/main/java/com/exercicio/array/matriz/ApplicationProgramming.java
@@ -0,0 +1,13 @@
+package com.exercicio.array.matriz;
+
+/**
+ * Faça um programa para armazenar em uma matriz os COMPROMISSOS de uma agenda pessoal. 
+ * CAda dia do mês deve conter 24 horas, onde para cada uma destas 24 horas podemos 
+ * associar uma tarefa específica(compromisso agendado). O programa deve ter um menu 
+ * onde o usuário indica o dia do mês QUE DESEJA ALTERAR E A HORA, entrando em seguida 
+ * com o compromisso, ou então, o usuário pode atambém consultar a AGENDA, 
+ * FORNECENDO O DIA E A HORA PARA OBTER O COMPROMISSO ARMAZENADO.
+ * */
+public class ApplicationProgramming {
+
+}
