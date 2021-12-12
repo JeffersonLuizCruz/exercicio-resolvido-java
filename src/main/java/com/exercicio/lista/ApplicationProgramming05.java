5732c16200878471b9c21665eea931a22b85402b
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-12-12 12:26:05 -0300

    Lista de Map

--- src/main/java/com/exercicio/lista/ApplicationProgramming05.java
+++ src/main/java/com/exercicio/lista/ApplicationProgramming05.java
@@ -4,27 +4,40 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+/**
+ * Essa atividade é para entender como funciona Map<String, List<String>>
+ * O objetivo dessa atividade é ver se um determinado valor existe dentro do map.
+ * */
 public class ApplicationProgramming05 {
 	public static void main(String[] args) {
 		
 		String cidade = "São Paulo, Recife, Rio de Janeiro, Campinas, Paraíba, Alagoas";
 		String[] splitCidade = cidade.split(",");
 		
-		String nome = "Jefferson, Hugo, Luiz, Carol, Jeff, Ana";
-		String[] splitNome = nome.split(",");
+		String nome1 = "Jefferson, Hugo, Luiz, Carol, Jeff, Ana";
+//		String nome2 = "Fulano, Ciclano, Beutrano, Tadeu, Jubelina, Serafina";
+//		String nome3 = "Matiude, Froide, Brumenaudo, Tiringa, Fabula, Cassiude";
+		
+		String[] splitNome1 = nome1.split(",");
+		
 		
 		Map<String, List<String>> map = new HashMap<>();
-		List<String> listCidades = new ArrayList<>();
+		List<String> listNomes1 = new ArrayList<>();
 		
-		for(String valueCidade : splitCidade) {
-			listCidades.add(valueCidade);
-			for(String valueNome : splitNome) {
-				map.put(valueNome, listCidades);
-			}
+		for(int i = 0; i < splitCidade.length; i++) {
+			listNomes1.add(splitNome1[i].trim());
+			map.put(splitCidade[i].trim(), listNomes1);
 		}
 		
-		System.out.println(map.entrySet());
+		//Verifica se um determinado valor existe dentro do mapa.
+		if(map.get("Campinas").contains("Hugo")) {
+			System.out.println("Esse mapa contem esse nome");
+		}else {
+			System.out.println("Não existe esse nome");
+		}
+		
+		System.out.printf("%s", map.entrySet());
+		
 	}
 
 }
