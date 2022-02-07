f10d1c63805f86325aab6d53ffd7be6aa937d5e9
Jefferson <jefferson.luiz.cruz@gmail.com>
2022-02-06 21:17:08 -0300

    Reader

--- src/main/java/com/exercicio/lerarquivo/ApplicationProgramming01.java
+++ src/main/java/com/exercicio/lerarquivo/ApplicationProgramming01.java
@@ -3,16 +3,18 @@ package com.exercicio.lerarquivo;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 
 public class ApplicationProgramming01 {
 	public static void main(String[] args) throws IOException {
+		// Também é possível usar a entrada de dados usando o inputStream e Reader. Elas são classes abstratadas e extentidas por FileInputStream e InputStreamReader
+//		InputStream file = new FileInputStream("tamplate.csv");
+//		Reader isr = new InputStreamReader(file); // transforma byts em caracteres
 		
-		InputStream file = new FileInputStream("tamplate.csv");
-		Reader isr = new InputStreamReader(file); // transforma byts em caracteres
-		BufferedReader br = new BufferedReader(isr); // ler os caracteres e manipula
+		
+		FileInputStream file = new FileInputStream("tamplate.csv");
+		InputStreamReader isr = new InputStreamReader(file); // transforma byts em caracteres
+		BufferedReader br = new BufferedReader(isr); // ler os caracteres e manipula	
 		
 		String readLine = br.readLine();
 		
