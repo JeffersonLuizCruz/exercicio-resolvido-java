34344f37868d3f3633d96d14976674f7cebedcb5
Jefferson <jefferson.luiz.cruz@gmail.com>
2022-02-06 21:11:04 -0300

    inputStream

--- src/main/java/com/exercicio/lerarquivo/ApplicationProgramming01.java
+++ src/main/java/com/exercicio/lerarquivo/ApplicationProgramming01.java
@@ -3,16 +3,23 @@ package com.exercicio.lerarquivo;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class ApplicationProgramming01 {
 	public static void main(String[] args) throws IOException {
 		
-		FileInputStream file = new FileInputStream("tamplate.csv");
-		InputStreamReader isr = new InputStreamReader(file); // transforma byts em caracteres
+		InputStream file = new FileInputStream("tamplate.csv");
+		Reader isr = new InputStreamReader(file); // transforma byts em caracteres
 		BufferedReader br = new BufferedReader(isr); // ler os caracteres e manipula
 		
-		System.out.println(br.readLine());
+		String readLine = br.readLine();
+		
+		while(readLine != null) {
+			System.out.println(readLine);
+			readLine = br.readLine();
+		}
 		br.close();
 	}
 
